2baee360cd1f7a111fbc52ff02adce247071f5f7
Gilbertoans <anschaugilberto@gmail.com>
2021-06-23 16:05:49 -0300

    fim aula 3

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*Menu abre e fecha */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -13,7 +14,7 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
-
+/* efeito do header */
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
@@ -24,3 +25,32 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/*Carousel testimonials swiper*/
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* ScrollReveal mostra elementos quando der scroll na pagina*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
